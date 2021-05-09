e05a25c58b7c03376fe93a415635966bdf5ad822
maemae9312 <hc081110@gmail.com>
2021-05-09 12:32:46 +0900

    Fix

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,7 @@ class Tweet < ApplicationRecord
 
   def self.search(search)
     if search != ""
-      Tweet.where('text LIKE(?)', "%#{search}%'")
+      Tweet.where('text LIKE(?)', "%#{search}%")
     else
       Tweet.all
     end
